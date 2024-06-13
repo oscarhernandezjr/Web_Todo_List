9f378ec7754d8fc77ec605d5478fe4f7c4e68598
oscar.hernandez <oscar.hernandez@stmartin.edu>
2024-06-13 15:25:27 -0700

    updated

---
+++ script.js
@@ -0,0 +1,34 @@
+document
+  .getElementById("todo-form")
+  .addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    // Get the value of the input field
+    let taskInput = this.querySelector('input[type="text"]');
+    let taskText = taskInput.value.trim();
+
+    if (taskText !== "") {
+      // Create a new list item
+      let li = document.createElement("li");
+      li.innerHTML = `<div class="todo-item">
+                            <span>${taskText}</span>
+                            <button class="remove" type="button">Remove</button>
+                        </div>`;
+
+      // Append li to the todo list
+      document.getElementById("todo-list").appendChild(li);
+
+      // Clear the input field
+      taskInput.value = "";
+    }
+  });
+
+document
+  .getElementById("todo-list")
+  .addEventListener("click", function (event) {
+    if (event.target.classList.contains("remove")) {
+      // Remove the parent <li> element
+      let li = event.target.closest("li");
+      li.remove();
+    }
+  });
